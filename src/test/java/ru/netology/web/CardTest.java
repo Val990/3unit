f7119404660e41183f50c956d176a432c5aa9ce3
Valeriya <tmcxezsim5nn@mail.ru>
2021-11-17 19:56:54 +0300

    changed

--- src/test/java/ru/netology/web/CardTest.java
+++ src/test/java/ru/netology/web/CardTest.java
@@ -7,14 +7,14 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ChromeTest {
+public class CardTest {
 
-    WebDriver driver;
+    private WebDriver driver;
 
     @BeforeAll
     static void setupClass() {
@@ -23,25 +23,32 @@ class ChromeTest {
 
     @BeforeEach
     void setupTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("disable-infobars");
+        options.addArguments("--headless");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
         driver = new ChromeDriver();
     }
 
     @AfterEach
     void teardown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
     @Test
-    public void shouldSendForm() {
+     public void shouldSendForm() {
         driver.get("http://localhost:9999/");
-
         driver.findElement(By.cssSelector("[type='text']")).sendKeys("Иванов Иван"); //фио
         driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79999999999"); // тел
-
         driver.findElement(By.cssSelector(".checkbox__box")).click(); // галочка
         driver.findElement(By.cssSelector("button")).click(); //отправить
-        String actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+        String actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        assertEquals(expected, actualText, "Текст не совпадает");
+        assertEquals(expected, actualText.trim(), "Текст не совпадает");
     }
 }
