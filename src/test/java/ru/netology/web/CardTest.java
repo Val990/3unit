21f6b0d6d084389d18d914b0db4be7043726efaa
Valeriya <tmcxezsim5nn@mail.ru>
2021-11-17 18:03:30 +0300

    tests

--- src/test/java/ru/netology/web/CardTest.java
+++ src/test/java/ru/netology/web/CardTest.java
@@ -1,28 +1,52 @@
 package ru.netology.web;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class CardTest {
-    private WebDriver driver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ChromeTest {
+
+    WebDriver driver;
 
     @BeforeAll
-    static public void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/mac/chromedriver");
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
-    public void setUp() {
+    void setupTest() {
         driver = new ChromeDriver();
     }
 
     @AfterEach
-    public void tearDown() {
-        driver.quit();
-        driver = null;
+    void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void shouldSendForm() {
+        driver.get("http://localhost:9999/");
+
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Иванов Иван"); //фио
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79999999999"); // тел
+
+        driver.findElement(By.cssSelector(".checkbox__box")).click(); // галочка
+        driver.findElement(By.cssSelector("button")).click(); //отправить
+        String actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actualText, "Текст не совпадает");
     }
 }
